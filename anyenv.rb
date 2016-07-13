111a6b5e4085ae8d7062de44f0b2c415b57fa3ff
takezo13 <n.takezo.0825@gmail.com>
2016-07-14 02:11:18 +0900

    modify caveats for anyenv-update

--- anyenv.rb
+++ anyenv.rb
@@ -1,6 +1,6 @@
 class Anyenv < Formula
   desc "all in one for **envs"
-  homepage "https://github.com/riywo/anyenv"
+  homepage "https://github.com/r[<32;34;3Miywo/anyenv"
   head "https://github.com/riywo/anyenv.git"
 
   def install
@@ -9,11 +9,17 @@ class Anyenv < Formula
   end
 
   def caveats; <<-EOS.undent
-    To  enable anyenv, add this to your profile follow:
+    To  enable anyenv, add this to your profile:
 
       if which anyenv > /dev/null; then
         eval "$(anyenv init -)"
       fi
+
+    To add "anyenv update" command, run the following commands:
+
+      mkdir -p $(anyenv root)/plugins
+      git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+
     EOS
   end
 
